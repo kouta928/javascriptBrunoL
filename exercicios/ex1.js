9b528d7362e034da00491063efd53f30a4d8dfa8
Bruno Chiavenato <lopes928@gmail.com>
2023-10-17 10:25:07 -0300

    exercicio arrumado

--- exercicios/ex1.js
+++ exercicios/ex1.js
@@ -2,63 +2,79 @@
 //a) utilize objetos para criar os dados do veículo e os dados de locação
 //b) utilize funções para calcular o valor de locação baseado em diárias e km percorridas
 //c) utilize laços de repetição para cadastrar 3 veículos locados(é neste ponto que os cálculos serão realizados)
-//d)utilize laços de repetição para listar os dados já com os cálculos efetuados dos veículos
+//d) utilize laços de repetição para listar os dados já com os cálculos efetuados dos veículos
 
 
-// let carros = [
-//     carro
-// ];
+let prompt = require('prompt-sync')();
 
-// let carro = {
-//     modelo : "",
-//     ano : "",
-//     cor : ""
-// };
+let diaria, qtdDias, precoGasolina, totalDiaria, totalKm, total;
 
-// for(let i = 0; i < 5; i++){
-//     console.log(`Carro modelo: ${i}`);
-// }
+let modelo, ano, cor, placa, renavam, chassi;
 
-function calcularValorLocacao(veiculo, diarias, kmPercorridos) {
-    const valorDiaria = veiculo.valorDiaria * diarias;
-    const valorKm = veiculo.valorKm * kmPercorridos;
-    return valorDiaria + valorKm;
-}
+let veiculos = [];
 
-// Array para armazenar os veículos
-const veiculos = [
-    { marca: 'Ford', modelo: 'Fiesta', valorDiaria: 100, valorKm: 0.2 },
-    { marca: 'Toyota', modelo: 'Corolla', valorDiaria: 150, valorKm: 0.3 },
-    { marca: 'Honda', modelo: 'Civic', valorDiaria: 120, valorKm: 0.25 },
-];
+let nCadastro = prompt("Digite a quantidade de carros que quer cadastrar: ");
 
-// Array para armazenar as locações
-const locacoes = [];
+do{
+    modelo = prompt("Digite o modelo: ");
+    ano = prompt("Digite o ano: ");
+    cor = prompt("Digite a cor: ");
+    placa = prompt("Digite a placa: ");
+    renavam = prompt("Digite o renavam: ");
+    chassi = prompt("Digite o chassi: ");
+    diaria = prompt("Preço da diária: R$");
+    qtdDias = prompt("Quantidade de dias: ");
+    qtdKm = prompt("Quantos km a serem rodados: ");
+    precoGasolina = prompt("Preço do litro da gasolina: R$");
 
-// Função para cadastrar locações
-function cadastrarLocacao(veiculo, diarias, kmPercorridos) {
-    const valorLocacao = calcularValorLocacao(veiculo, diarias, kmPercorridos);
-    locacoes.push({
-        veiculo,
-        diarias,
-        kmPercorridos,
-        valorLocacao,
-    });
-}
+    (()=>{
+        totalDiaria = parseFloat(diaria) * parseFloat(qtdDias);
+        return totalDiaria;
+    })();
 
-// Cadastrar 3 locações usando um loop
-for (let i = 0; i < 3; i++) {
-    const veiculo = veiculos[i];
-    const diarias = Math.floor(Math.random() * 7) + 1; // Diárias aleatórias de 1 a 7
-    const kmPercorridos = Math.floor(Math.random() * 300) + 1; // Km aleatórios de 1 a 300
-    cadastrarLocacao(veiculo, diarias, kmPercorridos);
-}
+    (()=>{
+        totalKm = parseFloat(precoGasolina) * parseFloat(qtdKm);
+        return totalKm;
+    })();
 
-// Listar os dados das locações com cálculos
-for (const locacao of locacoes) {
-    console.log(`Veículo: ${locacao.veiculo.marca} ${locacao.veiculo.modelo}`);
-    console.log(`Diárias: ${locacao.diarias}`);
-    console.log(`Km percorridos: ${locacao.kmPercorridos} km`);
-    console.log(`Valor Total da Locação: R$ ${locacao.valorLocacao.toFixed(2)}`);
-    console.log('------------------------');
+    (()=>{
+        total = totalDiaria + totalKm;
+        return total;
+    })();
+
+    veiculos.push({
+        modelo : modelo,
+        ano : ano,
+        cor : cor,
+        registro :{
+            placa : placa,
+            renavam : renavam,
+            chassi : chassi
+        },
+        locacao : {
+            diaria : diaria,
+            qtdDias : qtdDias,
+            qtdKm : qtdKm,
+            precoGasolina : precoGasolina,
+            totalDiaria : totalDiaria,
+            totalKm : totalKm,
+            total : total
+        }
+    })
+
+} while (veiculos.length < nCadastro);
+
+for( n in veiculos){
+    console.log(`
+    Cadastro de carros de aluguel
+    Modelo: ${veiculos[n].modelo}
+    Ano: ${veiculos[n].ano}
+    Cor: ${veiculos[n].cor}
+    Placa: ${veiculos[n].registro.placa}
+    Renavam: ${veiculos[n].registro.renavam}
+    Chassi: ${veiculos[n].registro.chassi}
+    Valor da diária: R$ ${veiculos[n].locacao.totalDiaria}
+    Valor da gasolina total: ${veiculos[n].locacao.totalKm}
+    Valor total: R$ ${veiculos[n].locacao.total}
+    `);
 }
